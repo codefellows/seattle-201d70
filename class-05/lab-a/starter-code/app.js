931d86391bab910e84d02d430fd0f52e5d31dc36
Ryan Gallaway <rkgallaway@msn.com>
2020-12-07 08:34:14 -0800

    class-06 resources added

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -9,11 +9,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
+  var theSum = a +b;
+  var theMessage = `The sum of ${a} and ${b} is ${theSum}.`;
 
+  return [theSum, theMessage];
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7); //eslint-disable-line
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -27,11 +30,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+  var theProduct = a * b;
+  var theMessage = `The product of ${a} and ${b} is ${theProduct}.`;
 
+  return [theProduct, theMessage];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9); //eslint-disable-line
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -48,7 +54,21 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  // addition
+  var sumOfTwoNumbers;
+  var sumOfThreeNumbers;
+  // or another way
+  var sumTotalAllAtOnce;
+  var sumMessage = `${a} and ${b} and ${c} sum to ${sumOfThreeNumbers}.`;
 
+  // multiplication
+  var productOfTwoNumbers;
+  var productOfThreeNumbers;
+  // or another way
+  var productOfAllAtOnce;
+  var productMessage = `The product of ${a} and ${b} and ${c} is ${productOfThreeNumbers}.`;
+
+  return [];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
@@ -69,13 +89,18 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
 
+// Ryan - if we have three numbers like above, can we compare it to problem 3?  yes we can!
+// If we make that comparison, what will a, b, and c equal?
+
 function sumArray(sumArr) { //eslint-disable-line
+
+
 
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+// testSumArray(testArray); //eslint-disable-line
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -95,9 +120,9 @@ function multiplyArray(multArr) { //eslint-disable-line
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+// testMultiplyArray(testArray); //eslint-disable-line
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
 
@@ -123,6 +148,6 @@ function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+// testMultiplyAnyArray(testDynamicArray); //eslint-disable-line
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
--- class-06/README.md
+++ class-06/README.md
@@ -2,7 +2,41 @@
 
 <a id="top"></a>
 
+### Week One Feedback Review
+#### Pros and Truths
+- This is Hard!
+- safe environment!
+- pair programming for the win / collaboration in general / bouncing ideas around
+- Code review very helpful
+- Canvas submissions - due at 8:59am or 11:59pm - working late.  
+- stacked learning for the win
+#### Cons and Improvables
+- Hard time staying engaged 
+  - Latent knowledge? NOT a bad thing!  Perfecting what you've got and talking with others is a great way to manage this for now.  Week 2, Newer more challenging material is coming.
+- Long Lectures 
+  - hasn't been received harshly -good, but TRUTH the lectures have been long.  Week 2, you'll notice more foucs
+- Canvas is a bit clunky
+  - yes, and it hits different folks in different ways.... it's FREE and a best option considering what's out there
+
+#### In a nut shell:  
+> This week has been challenging, exhausting, and frustrating, but also encouraging, and fulfilling
+
+> So far Code Fellows has exceeded my expectations. The course content is well structured and thought out. There is an ample amount of engagement and support. It is top-notch.
+
+> The overall combination of elements -- all these elements are nested together and gradually build upon themselves.
+
+> I love the pace and the help that is available. I love the priority on it being OK for everyone to do things their own way and take care of themselves when necessary.
+
+> I feel a sense of community here and that means a lot.
+
+#### Instructor Reflection
+> Lectures have been very long.  We've struck a balance based on what question folks have had about all of the things and the entire group's understanding. Take aways:  People know they are safe.  We have "buy in" and the group can navigate this system.  BIG WIN.  The other side of this is that we've held some to longer lectures than they needed in this effort reinforce the group.  In week one this very much helps our community, I appreciate the entire group's patience and I am not sad by those net results.  In week 2 however, I will be more concise; our topics are more specific this week.  Bottom line:  we need to consume some difficult concepts and stil have time to do all the stuff and things!  All of the things we talked about in week one will help us on this journey.  Quite literally, the hard work done in week 1, makes week two a bit easier! 
+
+
 ## Lecture
+
+#### Image Usage Rights Matter
+- mentioned in class:  [unsplash.com](unsplash.com)
 
 ## Learning Objectives
 
