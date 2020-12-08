f430de1b241d51f45154a5e76874274ae9c712a5
Ryan Gallaway <rkgallaway@msn.com>
2020-12-08 12:34:25 -0800

    class-07 demo added

--- class-07/code-reveiw/js/app.js
+++ class-07/code-reveiw/js/app.js
@@ -1,43 +1,229 @@
 'use strict';
 
-
-
-function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-
 // global variables
-var hours = ['6am', '7am', '8am', '7pm'];
+// hours of operation
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-// 1st object - get it to work
+// elements from DOM -- lists
+var seattleList = document.getElementById('seattle');
+var tokyoList = document.getElementById('tokyo');
+var dubaiList  = document.getElementById('dubai');
+var parisList = document.getElementById('paris');
+var limaList = document.getElementById('lima');
 
-var seattleStore = {
+// object literals
+var seattle = {
   name: 'Seattle',
   min: 23,
   max: 65,
   avg: 6.3,
-  hourlySales: [],
+  hourlySalesArray: [],
   dailyTotal: 0,
-
-  //successfully get random number between min and max provided
-  getRandomNumber: function (){
+  getRandomCustomerCount: function(){
     return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
   },
-
-  // populate hourlySales array
-  calculateHourlySales(){
-    console.log(this.getRandomNumber());
+  populateHourlySales(){
+    // iteratively, we are going to get a random number of custoemrs.  multiply  number of customers by the average sales/hour, THEN add that hourly total to the hourlySalesArray
+    for (var i = 0; i < hours.length; i++){
+      var customersForTheHour = this.getRandomCustomerCount();
+      var hourlyTotal = Math.ceil(customersForTheHour * this.avg);
+      // this.hourlySalesArray.push(hourlyTotal);  //either way.  this or below
+      this.hourlySalesArray[i] = hourlyTotal;
+      this.dailyTotal += hourlyTotal;
+    }
   },
-  // render my list with total at the end
   render: function(){
-    this.calculateHourlySales();
+    this.populateHourlySales();
 
-    // proof I can get here!
-    console.log('inside render method');
-    //do thething
-
+    //  iteratively render alist item
+    for(var i = 0; i < hours.length; i++){
+      //create an element
+      var liElement = document.createElement('li');
+      // give it content
+      liElement.textContent = `${hours[i]}: ${this.hourlySalesArray[i]} cookies`;
+      // append it to the DOM
+      seattleList.appendChild(liElement);
+    }
+    // render the daily total
+    liElement = document.createElement('li');
+    // give it content
+    liElement.textContent = `Total: ${this.dailyTotal} cookies`;
+    // append it to the DOM
+    seattleList.appendChild(liElement);
   }
 };
 
-seattleStore.render();
+var tokyo = {
+  name: 'Tokyo',
+  min: 3,
+  max: 24,
+  avg: 1.2,
+  hourlySalesArray: [],
+  dailyTotal: 0,
+  getRandomCustomerCount: function(){
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+  populateHourlySales(){
+    // iteratively, we are going to get a random number of custoemrs.  multiply  number of customers by the average sales/hour, THEN add that hourly total to the hourlySalesArray
+    for (var i = 0; i < hours.length; i++){
+      var customersForTheHour = this.getRandomCustomerCount();
+      var hourlyTotal = Math.ceil(customersForTheHour * this.avg);
+      // this.hourlySalesArray.push(hourlyTotal);  //either way.  this or below
+      this.hourlySalesArray[i] = hourlyTotal;
+      this.dailyTotal += hourlyTotal;
+    }
+  },
+  render: function(){
+    this.populateHourlySales();
+
+    //  iteratively render alist item
+    for(var i = 0; i < hours.length; i++){
+      //create an element
+      var liElement = document.createElement('li');
+      // give it content
+      liElement.textContent = `${hours[i]}: ${this.hourlySalesArray[i]} cookies`;
+      // append it to the DOM
+      tokyoList.appendChild(liElement);
+    }
+    // render the daily total
+    liElement = document.createElement('li');
+    // give it content
+    liElement.textContent = `Total: ${this.dailyTotal} cookies`;
+    // append it to the DOM
+    tokyoList.appendChild(liElement);
+  }
+};
+
+var dubai = {
+  name: 'Dubai',
+  min: 11,
+  max: 38,
+  avg: 3.7,
+  hourlySalesArray: [],
+  dailyTotal: 0,
+  getRandomCustomerCount: function(){
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+  populateHourlySales(){
+    // iteratively, we are going to get a random number of custoemrs.  multiply  number of customers by the average sales/hour, THEN add that hourly total to the hourlySalesArray
+    for (var i = 0; i < hours.length; i++){
+      var customersForTheHour = this.getRandomCustomerCount();
+      var hourlyTotal = Math.ceil(customersForTheHour * this.avg);
+      // this.hourlySalesArray.push(hourlyTotal);  //either way.  this or below
+      this.hourlySalesArray[i] = hourlyTotal;
+      this.dailyTotal += hourlyTotal;
+    }
+  },
+  render: function(){
+    this.populateHourlySales();
+
+    //  iteratively render alist item
+    for(var i = 0; i < hours.length; i++){
+      //create an element
+      var liElement = document.createElement('li');
+      // give it content
+      liElement.textContent = `${hours[i]}: ${this.hourlySalesArray[i]} cookies`;
+      // append it to the DOM
+      dubaiList.appendChild(liElement);
+    }
+    // render the daily total
+    liElement = document.createElement('li');
+    // give it content
+    liElement.textContent = `Total: ${this.dailyTotal} cookies`;
+    // append it to the DOM
+    dubaiList.appendChild(liElement);
+  }
+};
+
+var paris = {
+  name: 'Paris',
+  min: 20,
+  max: 38,
+  avg: 2.3,
+  hourlySalesArray: [],
+  dailyTotal: 0,
+  getRandomCustomerCount: function(){
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+  populateHourlySales(){
+    // iteratively, we are going to get a random number of custoemrs.  multiply  number of customers by the average sales/hour, THEN add that hourly total to the hourlySalesArray
+    for (var i = 0; i < hours.length; i++){
+      var customersForTheHour = this.getRandomCustomerCount();
+      var hourlyTotal = Math.ceil(customersForTheHour * this.avg);
+      // this.hourlySalesArray.push(hourlyTotal);  //either way.  this or below
+      this.hourlySalesArray[i] = hourlyTotal;
+      this.dailyTotal += hourlyTotal;
+    }
+  },
+  render: function(){
+    this.populateHourlySales();
+
+    //  iteratively render alist item
+    for(var i = 0; i < hours.length; i++){
+      //create an element
+      var liElement = document.createElement('li');
+      // give it content
+      liElement.textContent = `${hours[i]}: ${this.hourlySalesArray[i]} cookies`;
+      // append it to the DOM
+      parisList.appendChild(liElement);
+    }
+    // render the daily total
+    // create element
+    liElement = document.createElement('li');
+    // give it content
+    liElement.textContent = `Total: ${this.dailyTotal} cookies`;
+    // append it to the DOM
+    parisList.appendChild(liElement);
+  }
+};
+
+var lima = {
+  name: 'Lima',
+  min:  2,
+  max: 16,
+  avg: 4.6,
+  hourlySalesArray: [],
+  dailyTotal: 0,
+  getRandomCustomerCount: function(){
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+  populateHourlySales(){
+    // iteratively, we are going to get a random number of custoemrs.  multiply  number of customers by the average sales/hour, THEN add that hourly total to the hourlySalesArray
+    for (var i = 0; i < hours.length; i++){
+      var customersForTheHour = this.getRandomCustomerCount();
+      var hourlyTotal = Math.ceil(customersForTheHour * this.avg);
+      // this.hourlySalesArray.push(hourlyTotal);  //either way.  this or below
+      this.hourlySalesArray[i] = hourlyTotal;
+      this.dailyTotal += hourlyTotal;
+    }
+  },
+  render: function(){
+    this.populateHourlySales();
+
+    //  iteratively render alist item
+    for(var i = 0; i < hours.length; i++){
+      //create an element
+      var liElement = document.createElement('li');
+      // give it content
+      liElement.textContent = `${hours[i]}: ${this.hourlySalesArray[i]} cookies`;
+      // append it to the DOM
+      limaList.appendChild(liElement);
+    }
+    // render the daily total
+    liElement = document.createElement('li');
+    // give it content
+    liElement.textContent = `Total: ${this.dailyTotal} cookies`;
+    // append it to the DOM
+    limaList.appendChild(liElement);
+  }
+};
+
+
+// executables
+seattle.render();
+tokyo.render();
+dubai.render();
+paris.render();
+lima.render();
+
+
--- class-07/demo/inclass-demo/js/app.js
+++ class-07/demo/inclass-demo/js/app.js
@@ -4,15 +4,24 @@ var containerParent = document.getElementById('container-parent');
 var impeyanList = document.getElementById('impeyan');
 var blueList = document.getElementById('blue-earred');
 var reevesList = document.getElementById('reeves');
+var pheasantTable = document.getElementById('table');
+var pheasants = [];
 // console.log(impeyanList, blueList, reevesList);
 
-var impeyan = {
-  title: 'Impeyan, Male',
-  src: 'img/impeyan.jpg',
-  alt: 'Male Impeyan Pheasant with brilliant blue, green, black, and orange colors standing in grass near rocks',
-  colors: ['Blue', 'Orange', 'Green'],
-  pContent: 'The Himalayan Monal, Lophophorus impejanus also known as the Impeyan Monal, Impeyan Pheasant, and Danphe, is a bird in the pheasant family, Phasianidae. It is the national bird of Nepal. the Himalayan Monal has been classified as monophyletic. However, studies have shown that the male Himalayan Monal of northwestern India lacks the white rump of other Himalayan Monals, and it has more green on the breast, indicating the possibility of a second subspecies.',
-  render: function () {
+//paragraph blurbs
+var impeyanBlurb = 'The Himalayan Monal, Lophophorus impejanus also known as the Impeyan Monal, Impeyan Pheasant, and Danphe, is a bird in the pheasant family, Phasianidae. It is the national bird of Nepal. the Himalayan Monal has been classified as monophyletic. However, studies have shown that the male Himalayan Monal of northwestern India lacks the white rump of other Himalayan Monals, and it has more green on the breast, indicating the possibility of a second subspecies.';
+
+var blueEarredBlurb = 'Blue Eared Pheasants are a rich blue-gray color, but with bright red facial patches and distinguishing white ear coverts along the sides of the head. The top of the head is highlighted by dark blue-black coloring that matches the darker blue-gray of the full tail. Tails are made up of 24 gracefully flowing feathers. Legs and feet are scarlet and match the facial patches.  Blue Eared Pheasants are vocal and amiable birds. Although pheasants in general tend to be a bit standoffish, Blue Eared Pheasants are one of the friendliest of species and have been known to approach their caretakers and eat out of their hands.';
+
+var reevesBlurb = 'Reeves\'s pheasant is a species unique to central and eastern China. With its long tail, the bird is speedy both while flying and on the ground. During fast flight, the long tails help the bird control its direction and buffers its landing. Female birds usually appear to be very attached to their nests. They are reluctant to leave and make intimidating poses even if humans approach the nests.';
+
+function Pheasant(title, src, colors, blurb) {
+  this.title = title;
+  this.src = src;
+  this.colors = colors;
+  this.pContent = blurb;
+  pheasants.push(this);
+  this.render = function () {
     // create element
     var sectionElement = document.createElement('section');
 
@@ -46,150 +55,50 @@ var impeyan = {
     pElement.textContent = this.pContent;
     //append it to the DOM
     articleElement.appendChild(pElement);
-  },
-  renderList: function () {
-    for (var i = 0; i < this.colors.length; i++) {
-      // create element
-      var liElement = document.createElement('li');
-      // give it content
-      liElement.textContent = this.colors[i];
-      // append it to the DOM
-      impeyanList.appendChild(liElement);
-    }
-    liElement = document.createElement('li');
-    // give it content
-    liElement.textContent = 'the total';
-    // append it to the DOM
-    impeyanList.appendChild(liElement);
+  };
+}
+
+//functions / prototype methods
+Pheasant.prototype.renderTable = function () {
+  // create Row and append to the DOM
+  var trElement = document.createElement('tr');
+  pheasantTable.appendChild(trElement);
+
+  // create the first cell in row, give it the name content, and append it to the DOM
+  var thElement = document.createElement('th');
+  thElement.textContent = this.title;
+  trElement.appendChild(thElement);
+
+  // iterativly create, give color content, and append to the DOM
+  for (var i = 0; i < this.colors.length; i++) {
+    var tdElement = document.createElement('td');
+    tdElement.textContent = this.colors[i];
+    trElement.appendChild(tdElement);
   }
+
+  //finally, add the total to the end of the row
+  tdElement = document.createElement('td');
+  tdElement.textContent = this.colors.length;
+  trElement.appendChild(tdElement);
 };
 
-var blueEarred = {
-  title: 'BlueEarred, Male',
-  src: 'img/blue-earred.jpg',
-  alt: 'BLue Earred relevant message',
-  colors: ['Blue', 'Black', 'Red'],
-  pContent: 'Blue Eared Pheasants are a rich blue-gray color, but with bright red facial patches and distinguishing white ear coverts along the sides of the head. The top of the head is highlighted by dark blue-black coloring that matches the darker blue-gray of the full tail. Tails are made up of 24 gracefully flowing feathers. Legs and feet are scarlet and match the facial patches.  Blue Eared Pheasants are vocal and amiable birds. Although pheasants in general tend to be a bit standoffish, Blue Eared Pheasants are one of the friendliest of species and have been known to approach their caretakers and eat out of their hands.',
-  render: function () {
-    // create element
-    var sectionElement = document.createElement('section');
+//top and bottom rows of table are missing, this isn't built out yet!
+function renderHeader(){
+  // in Salmon Cookies, mkae this look like image in lab-07 instructions:  iterate thru the hours array
+}
 
-    // give it content
-    // its content will be other elements - created later
+// instantiations
+new Pheasant('Impeyan', 'img/impeyan.jpg', ['Blue', 'Green', 'Copper'], impeyanBlurb);
+new Pheasant('Blue Earred', 'img/blue-earred.jpg', ['Blue', 'Black', 'White'], blueEarredBlurb);
+new Pheasant('Reeves', 'img/reeves-pair.png', ['Gold', 'Black', 'White'], reevesBlurb);
 
-    // append to the DOM
-    containerParent.appendChild(sectionElement);
+function renderAll(){
+  for (var i = 0; i < pheasants.length; i++){
+    pheasants[i].render();
+    pheasants[i].renderTable();
 
-    //create element
-    var imgElement = document.createElement('img');
-    // give it content
-    imgElement.setAttribute('src', this.src);
-    // append it to the DOM
-    sectionElement.appendChild(imgElement);
-
-    //create element, append it to DOM, give content later
-    var articleElement = document.createElement('article');
-    sectionElement.appendChild(articleElement);
-
-    //create element
-    var h2Element = document.createElement('h2');
-    // give it content
-    h2Element.textContent = this.title;
-    //append it to the DOM
-    articleElement.appendChild(h2Element);
-
-    //create element
-    var pElement = document.createElement('p');
-    // give it content
-    pElement.textContent = this.pContent;
-    //append it to the DOM
-    articleElement.appendChild(pElement);
-  },
-  renderList: function () {
-    for (var i = 0; i < this.colors.length; i++) {
-      // create element
-      var liElement = document.createElement('li');
-      // give it content
-      liElement.textContent = this.colors[i];
-      // append it to the DOM
-      blueList.appendChild(liElement);
-    }
-    liElement = document.createElement('li');
-    // give it content
-    liElement.textContent = 'the total';
-    // append it to the DOM
-    blueList.appendChild(liElement);
   }
-};
+}
 
-var reeves = {
-  title: 'Reeves, Pair',
-  src: 'img/reeves-pair.png',
-  alt: 'Reeves Pair relevant message',
-  colors: ['Gold', 'Black', 'White'],
-  pContent: 'Reeves\'s pheasant is a species unique to central and eastern China. With its long tail, the bird is speedy both while flying and on the ground. During fast flight, the long tails help the bird control its direction and buffers its landing. Female birds usually appear to be very attached to their nests. They are reluctant to leave and make intimidating poses even if humans approach the nests.',
-  render: function () {
-    // create element
-    var sectionElement = document.createElement('section');
-
-    // give it content
-    // its content will be other elements - created later
-
-    // append to the DOM
-    containerParent.appendChild(sectionElement);
-
-    //create element
-    var imgElement = document.createElement('img');
-    // give it content
-    imgElement.setAttribute('src', this.src);
-    // append it to the DOM
-    sectionElement.appendChild(imgElement);
-
-    //create element, append it to DOM, give content later
-    var articleElement = document.createElement('article');
-    sectionElement.appendChild(articleElement);
-
-    //create element
-    var h2Element = document.createElement('h2');
-    // give it content
-    h2Element.textContent = this.title;
-    //append it to the DOM
-    articleElement.appendChild(h2Element);
-
-    //create element
-    var pElement = document.createElement('p');
-    // give it content
-    pElement.textContent = this.pContent;
-    //append it to the DOM
-    articleElement.appendChild(pElement);
-  },
-  renderList: function () {
-    for (var i = 0; i < this.colors.length; i++) {
-      // create element
-      var liElement = document.createElement('li');
-      // give it content
-      liElement.textContent = this.colors[i];
-      // append it to the DOM
-      reevesList.appendChild(liElement);
-    }
-    liElement = document.createElement('li');
-    // give it content
-    liElement.textContent = 'the total';
-    // append it to the DOM
-    reevesList.appendChild(liElement);
-  }
-};
-
-// how to manipulate DOM
-// create element
-// give it content
-// append it to the DOM
-
-impeyan.render();
-blueEarred.render();
-reeves.render();
-
-impeyan.renderList();
-blueEarred.renderList();
-reeves.renderList();
-
+// executable code
+renderAll();
